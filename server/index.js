76d50bf6e022151fd4b13f0fd685eb96ee4493be
Elvin Garcia <elvingarcia19@gmail.com>
2023-04-03 21:51:21 -0400

    created post route

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
-
 const cors = require("cors")
+const pool = require("./db")
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+//Routes
+
+//Create a todo
+app.post("/todos", async(req, res) => {
+    try {
+
+        const { description } = req.body
+        const newTodo = await pool.query(
+            "INSERT INTO todos (description) VALUES($1) RETURN *", // This will insert the data into the table we created on our database
+            [description]
+        );
+        res.json(newTodo.rows[0])
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 app.listen(8000, () => console.log("Server is runnong on port 8000"))
