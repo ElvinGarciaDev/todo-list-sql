b7ce4592b60d53f6705c56f71e292b429c631253
Elvin Garcia <elvingarcia19@gmail.com>
2023-04-03 22:29:20 -0400

    backend done

--- server/index.js
+++ server/index.js
@@ -37,4 +37,61 @@ app.get("/todos", async (req, res) => {
     }
 })
 
+//get Single todo
+app.get("/todos/:id", async (req, res) => {
+
+    try {
+        
+        const {id} = req.params // get the id from the url
+
+        // select a specific item from the todos table
+        const todo = await pool.query(
+            "SELECT * FROM todos WHERE id = $1",
+            [id]
+        );
+
+        res.json(todo.rows[0])
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+// Update a todo
+app.put("/todos/:id", async (req, res) => {
+
+    try {
+        const {id} = req.params // get the id from the url
+        const {description} = req.body // Get the new description that ill update the old
+    
+        // Update the description of a specific item from the todos table
+        await pool.query(
+            "UPDATE todos SET description = $1 WHERE id = $2",
+            [description, id]
+        )
+        res.json("Todo was updated")
+    
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.delete("/todos/:id", async (req, res) => {
+
+    try {
+        const {id} = req.params // get the id from the url
+
+        // Delete an item from the todos table
+        await pool.query(
+            "DELETE FROM todos WHERE id = $1",
+            [id]
+        )
+
+        res.json("Todo was deleted")
+        
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
 app.listen(8000, () => console.log("Server is runnong on port 8000"))
